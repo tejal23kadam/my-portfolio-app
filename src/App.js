7d1176fa4f4d7316e7740a346c22e6d54c054557
tejal23kadam <154776489+tejal23kadam@users.noreply.github.com>
2024-06-13 11:54:50 +0530

    added git repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav class="menu" tabindex="0">
+        <div class="smartphone-menu-trigger"></div>
+        <header class="avatar">
+          <img src="https://s3.amazonaws.com/uifaces/faces/twitter/kolage/128.jpg" />
+          <h2>John D.</h2>
+        </header>
+        <ul>
+          <li tabindex="0" class="icon-dashboard"><a href="#tabs-2" role="tab" data-toggle="pill">Free Cash Back Purchase Rewards</a></li>
+          <li tabindex="0" class="icon-customers"><a href="#tabs-3" role="tab" data-toggle="pill">Free Online Banking</a></li>
+          <li tabindex="0" class="icon-users"><span>Users</span></li>
+          <li tabindex="0" class="icon-settings"><span>Settings</span></li>
+        </ul>
+      </nav>
+
+      <main>
+        <div >
+          <div class="tab-pane" id="tabs-2">
+            <h2>Cash Back Purchase Rewards</h2>
+            <p>Make your everyday purchases a little less painful. With your X checking account, you'll rack up rewards just by using your Visa&reg; Debit card.
+              Personalized, cash back rewards. Nice.</p> <a href="/account-features#Get%20Cash%20Back">Learn More   »</a>
+          </div>
+          <div class="tab-pane" id="tabs-3">
+            <h2>Online Banking</h2>
+            <p>Take banking off of your chore list. Now you can do your banking from anywhere, anytime, for free. That's an easy way to keep your money and your time where it belongs - with you.</p> <a href="/account-features#Online Banking">Learn More   »</a>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
+
+
 
 export default App;
