c4ebb3665cdd692026b97c199ae165b732da6bb8
tejal23kadam <154776489+tejal23kadam@users.noreply.github.com>
2024-06-24 12:08:14 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,71 @@
 import './App.css';
+import profileImage from './images/profile-image.jpg';
 function App() {
   return (
-    <div>
-      <nav class="menu" tabindex="0">
-        <div class="smartphone-menu-trigger"></div>
-        <header class="avatar">
-          <img src="https://s3.amazonaws.com/uifaces/faces/twitter/kolage/128.jpg" />
-          <h2>John D.</h2>
+    <div>      
+      <nav className="menu" tabIndex="0">
+        <div className="smartphone-menu-trigger"></div>
+        <header className="avatar">
+          <img src={profileImage} alt="no image " />
+          <h2 className="name">Tejal Kadam
+            <span className="back">Tejal Kadam</span>
+          </h2>
         </header>
+
         <ul>
-          <li tabindex="0" class="icon-dashboard"><a href="#tabs-2" role="tab" data-toggle="pill">Free Cash Back Purchase Rewards</a></li>
-          <li tabindex="0" class="icon-customers"><a href="#tabs-3" role="tab" data-toggle="pill">Free Online Banking</a></li>
-          <li tabindex="0" class="icon-users"><span>Users</span></li>
-          <li tabindex="0" class="icon-settings"><span>Settings</span></li>
+          <li>
+            <a href="#section1">
+              <span className="circle"></span>
+              ONE
+            </a>
+          </li>
+          <li>
+            <a href="#section2">
+              <span className="rect"></span>
+              <span className="circle"></span>
+              TWO
+            </a>
+          </li>
+          <li>
+            <a href="#section3">
+              <span className="rect"></span>
+              <span className="circle"></span>
+              THREE
+            </a>
+          </li>
+          <li>
+            <a href="#section4">
+              <span className="rect"></span>
+              <span className="circle"></span>
+              FOUR
+            </a>
+          </li>
+          <li>
+            <a href="#section5">
+              <span className="rect"></span>
+              <span className="circle"></span>
+              FIVE
+            </a>
+          </li>
         </ul>
       </nav>
-
-      <main>
-        <div >
-          <div class="tab-pane" id="tabs-2">
-            <h2>Cash Back Purchase Rewards</h2>
-            <p>Make your everyday purchases a little less painful. With your X checking account, you'll rack up rewards just by using your Visa&reg; Debit card.
-              Personalized, cash back rewards. Nice.</p> <a href="/account-features#Get%20Cash%20Back">Learn More   »</a>
-          </div>
-          <div class="tab-pane" id="tabs-3">
-            <h2>Online Banking</h2>
-            <p>Take banking off of your chore list. Now you can do your banking from anywhere, anytime, for free. That's an easy way to keep your money and your time where it belongs - with you.</p> <a href="/account-features#Online Banking">Learn More   »</a>
-          </div>
-        </div>
-      </main>
+      
+      <section id="section1" className="content-section">
+        <h1>SECTION one</h1>
+      </section>
+      <section id="section2" className="content-section">
+        <h1>SECTION two</h1>
+      </section>
+      <section id="section3" className="content-section">
+        <h1>SECTION three</h1>
+      </section>
+      <section id="section4" className="content-section">
+        <h1>SECTION fpour</h1>
+      </section>
+      <section id="section5" className="content-section">
+        <h1>SECTION our</h1>
+      </section>
+    
     </div>
   );
 }
